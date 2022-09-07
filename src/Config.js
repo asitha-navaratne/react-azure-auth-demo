b92b5c7e2681b29510e0318ff13a4e4291c1079e
Asitha Navaratne <asithan476@gmail.com>
2022-09-08 01:12:19 +0530

    Config file created

---
+++ src/Config.js
@@ -0,0 +1,7 @@
+export const config = {
+  appId: "88c8b80f-4661-45c0-b99b-5b09fa8efc7b",
+  redirectUrl: "http://localhost:3000",
+  scopes: ["user.read"],
+  authority:
+    "https://login.microsoftonline.com/asithan476gmail.onmicrosoft.com",
+};
