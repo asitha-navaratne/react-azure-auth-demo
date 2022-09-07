9c252326ddd21b386352f04a33d28d619a9b70be
Asitha Navaratne <asithan476@gmail.com>
2022-09-08 02:11:52 +0530

    Changed env file

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const config = {
-  appId: process.env.AZURE_APP_ID,
+  appId: process.env.REACT_APP_AZURE_APP_ID,
   redirectUrl: "http://localhost:3000",
   scopes: ["user.read"],
-  authority: process.env.AZURE_AUTHORITY,
+  authority: process.env.REACT_APP_AZURE_AUTHORITY,
 };
